6c4980ee4a07879d453ae7bba3e2fad10020f226
Ro'uf Aufalin Al Ghoitsal <roufaufal01@gmail.com>
2025-01-07 13:04:22 +0700

    delete unused repository in main activity

--- app/src/main/java/com/bangkit/capstoneandroidexpert/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstoneandroidexpert/MainActivity.kt
@@ -13,14 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
-    @Inject
-    lateinit var repository : com.bangkit.core.data.ProductRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
